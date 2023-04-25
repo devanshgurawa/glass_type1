ec31f8f4b47265fecd053bfc8ea380707c2701f9
devanshgurawa <130561264+devanshgurawa@users.noreply.github.com>
2023-04-25 16:45:10 +0530

    Update glass_type.py

--- glass_type.py
+++ glass_type.py
@@ -80,11 +80,23 @@ for feature in features_list:
     sns.scatterplot(x=feature, y='GlassType', data=glass_df)
     st.pyplot()
 
+st.sidebar.subheader('Histogram')
+hist_features = st.sidebar.multiselect('Select the x-axis Values', ('RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba' , 'Fe'))
 
+for feature in hist_features:
+    st.subheader(f'Histogram Between {feature} and Glass Type')
+    plt.figure(figsize=(16,7))
+    plt.hist(glass_df[feature], edgecolor='black')
+    st.pyplot()
 
+st.sidebar.subheader('Box Plot')
+box_features = st.sidebar.multiselect('Select the x-axis Values', ('RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba' , 'Fe'))
 
-
-
+for feature in box_features:
+    st.subheader(f'Box Plot for {feature}')
+    plt.figure(figsize=(16,7))
+    sns.boxplot(glass_df[feature])
+    st.pyplot()
 
 
   
