e750adeb36ca4f58ea2eca422365ef9cd101a631
devanshgurawa <130561264+devanshgurawa@users.noreply.github.com>
2023-05-01 16:37:36 +0530

    Update glass_type.py

--- glass_type.py
+++ glass_type.py
@@ -134,7 +134,7 @@ if 'correlation heat map' in plot_types:
     st.pyplot()
 
 if 'pair plot' in plot_types:
-    st.subheader('Pair Plot)
+    st.subheader('Pair Plot')
     columns = st.sidebar.selectbox('Select the x-axis Values', ('RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba' , 'Fe'))
     plt.figure(figsize=(12,6))
     plt.title(f"Pair Plot for {columns}")
